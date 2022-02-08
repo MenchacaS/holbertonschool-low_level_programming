9b40ee53196a70decf2dfe66a8ac57480290cd51
MenchacaS <3615@holbertonschool.com>
2022-02-08 23:38:18 +0000

    sup

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+  * add - sike
+  * @i1: The first number
+  * @i2: The second number
+  *
+  * Return: Integer
+  */
+int add(int i1, int i2)
+{
+	return (i1 + i2);
+}
