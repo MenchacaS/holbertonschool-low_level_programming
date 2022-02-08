9ce0ab5a7c3b4b6ebe951c2276243d470629b4c2
MenchacaS <3615@holbertonschool.com>
2022-02-08 23:23:37 +0000

    sup

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _islower - lowercase character
+  * @c: character to be checked
+  *
+  * Return: 1 for something
+  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+
