5de0ad62c93cc6c4e1dacb310ba82663e178d7a1
MenchacaS <3615@holbertonschool.com>
2022-02-10 23:03:25 +0000

    sup

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - cool stuff
+  *
+  * Return: Numbers
+  */
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	for (; a <= 9; a++)
+	{
+		if (a == 2 || a == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(a + '0');
+		}
+	}
+
+	_putchar('\n');
+}
