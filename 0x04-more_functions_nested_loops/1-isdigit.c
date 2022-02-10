4ef65384a399e4de2ffb259eab78a7c6782f58fd
MenchacaS <3615@holbertonschool.com>
2022-02-10 21:42:33 +0000

    sup

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _isdigit - character is a digit
+  * @i: checked
+  *
+  * Return: 1
+  */
+int _isdigit(int i)
+{
+	if (i >= 48 && i <= 57)
+	{
+		return (1);
+	}
+
+	return (0);
+}
