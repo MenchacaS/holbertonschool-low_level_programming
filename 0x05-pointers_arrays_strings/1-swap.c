a4239ca770de4c90a29b068e7665bbc933367a26
MenchacaS <3615@holbertonschool.com>
2022-02-14 14:36:40 +0000

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - swaper 1000
+ * @a: int abe
+ * @b: int babe
+ * Return: emote
+ */
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+	return;
+
+}
