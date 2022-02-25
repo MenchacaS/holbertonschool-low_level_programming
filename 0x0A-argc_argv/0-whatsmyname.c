b1eaf9b2948dd15e75a455859fddf03c3b07af68
MenchacaS <3615@holbertonschool.com>
2022-02-25 18:15:56 +0000

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Prints
+  * @argc: count
+  * @argv: argumento
+  *
+  * Return: 0
+  */
+
+int main(int argv, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
